81f15195303b44d2986e9fba53519f62354c37fb
Eduardo <eduardodmdev@icloud.com>
2023-01-23 12:57:03 +0100

    Initial Commit

---
+++ FactoryPatternInAugmentedReality/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  FactoryPatternInAugmentedReality
+//
+//  Created by Eduardo Developer on 23/1/23.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
